4b7be9b6e9f54de3793b80fdeafb1855810f481b
Kundan <kundanss22@gmail.com>
2025-04-12 19:17:21 +0530

    add suspense

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -2,10 +2,16 @@ import { Book } from "@/types";
 import React from "react";
 import BookCard from "./BookCard";
 
-const BookList = ({ books }: { books: Book[] }) => {
+const BookList = async () => {
+  const response = await fetch(`${process.env.BACKEND_URL}/books`);
+  if (!response.ok) {
+    throw new Error("error occured while fetching the books.");
+  }
+  const { books } = await response.json();
+
   return (
     <div className="mx-auto max-w-7xl py-10 grid grid-cols-1 md:grid-cols-3 gap-8 ">
-      {books?.map((book) => {
+      {books?.map((book: Book) => {
         return <BookCard key={book._id} book={book} />;
       })}
     </div>
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,15 @@
 import Banner from "@/app/(home)/components/Banner";
 import BookList from "./components/BookList";
+import { Suspense } from "react";
+import Loader from "@/components/Loader";
 
 export default async function Home() {
-  const response = await fetch(`${process.env.BACKEND_URL}/books`);
-  if (!response.ok) {
-    throw new Error("error occured while fetching the books.");
-  }
-  const books = await response.json();
-  console.log(books.books);
   return (
     <div>
       <Banner />
-      <BookList books={books.books} />
+      <Suspense fallback={<Loader />}>
+        <BookList />
+      </Suspense>
     </div>
   );
 }
